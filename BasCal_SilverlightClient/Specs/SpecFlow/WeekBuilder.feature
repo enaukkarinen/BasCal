689ccafe1e466f79433a40901d92dd1b9d8bcf0f
Einari Naukkarinen <einarinau@basware.com>
2014-08-11 15:04:47 +0300

    final

---
+++ BasCal_SilverlightClient/Specs/SpecFlow/WeekBuilder.feature
@@ -0,0 +1,10 @@
+﻿Feature: WeekBuilder
+	In order to avoid silly mistakes
+	As a math idiot
+	I want to be told the sum of two numbers
+
+@mytag
+Scenario: Get events for a specified month
+	Given I have given a valid "month" and a "collection of events"
+	When I have given a valid month
+	Then I should get a see month filled with events
---
+++ BasCal_SilverlightClient/Specs/SpecFlow/WeekBuilderSteps.cs
@@ -0,0 +1,44 @@
+﻿using BasCal_SilverlightClient.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using TechTalk.SpecFlow;
+
+namespace Specs.SpecFlow
+{
+    [Binding]
+    public class WeekBuilderSteps
+    {
+
+        [StepArgumentTransformation("month")]
+        public DateTime TransformToday()
+        {
+            return DateTime.Today;
+        }
+
+        [Given("I have given a valid (.*) and a (.*)")]
+        public void GivenValidMonthAndEventCollection(DateTime month)
+        {
+
+            ScenarioContext.Current.Pending();
+        }
+
+        [When("I have given a valid month")]
+        public void WhenIPressAdd()
+        {
+            //TODO: implement act (action) logic
+
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then("I should get a see month filled with events")]
+        public void ThenTheResultShouldBe(ObservableCollection<Week> result)
+        {
+            //TODO: implement assert (verification) logic
+
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
